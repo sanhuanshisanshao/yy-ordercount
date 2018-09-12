684368a36f65591bc7ea6605e80e7df3c6475d52
gaoq <gaoq@weipaitang.com>
2018-09-12 11:49:22 +0800

    期号选择优化

--- auto-buy/baseinfo/baseInfo.go
+++ auto-buy/baseinfo/baseInfo.go
@@ -1,19 +1,20 @@
 package baseinfo
 
 type ids struct {
+	Area string
 	Gcid int
 	Gpid int
 }
 
-var UniqueAreaIds map[string]ids //地区ID
+var UniqueAreaIds []ids //地区ID
 
 func NewAreaIds() {
-	UniqueAreaIds = make(map[string]ids)
-	UniqueAreaIds["guangdong"] = ids{Gcid: 1, Gpid: 1}
-	UniqueAreaIds["shanghai"] = ids{Gcid: 3, Gpid: 11}
-	UniqueAreaIds["zhejiang"] = ids{Gcid: 8, Gpid: 21}
-	UniqueAreaIds["fujian"] = ids{Gcid: 12, Gpid: 31}
-	UniqueAreaIds["shandong"] = ids{Gcid: 2, Gpid: 6}
-	UniqueAreaIds["jiangsu"] = ids{Gcid: 5, Gpid: 16}
-	UniqueAreaIds["jiangxi"] = ids{Gcid: 11, Gpid: 26}
+	UniqueAreaIds = make([]ids, 7)
+	UniqueAreaIds[0] = ids{Area: "jiangsu", Gcid: 5, Gpid: 16}
+	UniqueAreaIds[1] = ids{Area: "jiangxi", Gcid: 11, Gpid: 26}
+	UniqueAreaIds[2] = ids{Area: "zhejiang", Gcid: 8, Gpid: 21}
+	UniqueAreaIds[3] = ids{Area: "shanghai", Gcid: 3, Gpid: 11}
+	UniqueAreaIds[4] = ids{Area: "fujian", Gcid: 12, Gpid: 31}
+	UniqueAreaIds[5] = ids{Area: "guangdong", Gcid: 1, Gpid: 1}
+	UniqueAreaIds[6] = ids{Area: "shandong", Gcid: 2, Gpid: 6}
 }
